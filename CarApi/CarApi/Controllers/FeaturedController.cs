31d48e7a41c99dac62117ccd8d6e37f4a6e68bf8
Sivanandham D <v-sivad@microsoft.com>
2019-05-21 19:38:51 +0530

    changes in featured controller

--- CarApi/CarApi/Controllers/FeaturedController.cs
+++ CarApi/CarApi/Controllers/FeaturedController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CarApi.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
@@ -11,6 +12,7 @@ namespace CarApi.Controllers
 {
     [Route("api/featured")]
     [ApiController]
+    //[Authorize]
     public class FeaturedController : ControllerBase
     {
         private CarData _cardata;
